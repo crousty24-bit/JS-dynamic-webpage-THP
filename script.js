96e04f095deb16de0dc779dec6007950f989e6aa
crousty24-bit <allen.kch24@gmail.com>
2026-01-22 11:44:33 +0100

    add first func event to count clicks on footer

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1. When user click on footer : display "click" in console
+const footer = document.querySelector("footer");
+footer.addEventListener("click", function(e){
+  console.log("click");
+});
+
+// 1.1 Update with count each time footer is clicked
+const footerCount = document.querySelector("footer");
+let count = 0;
+footerCount.addEventListener("click", function(e){
+    count++;
+    console.log(`Click N° ${count}`);
+});
+
+// 2.
+const btn = document.querySelector(".navbar-toggler");
+const nav = document.querySelector("#navbarHeader")
+console.log(btn);
+console.log(nav);
+btn.addEventListener("click", function(e){
+  nav.classList.toggle("collapse");
+});
